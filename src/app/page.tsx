dd7135b1b26d0566b939c18c446e28de6ae6a9af
Pasin0931 <pasin0931@gmail.com>
2025-08-13 19:19:53 +0700

    Add localStorage to store user and authToken

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from "react"
 import AuthForm from "@/components/auth-form"
+import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
 
 interface UserData {
   id: string
@@ -12,12 +14,36 @@ interface UserData {
 export default function Home() {
 
   const [user, setUser] = useState<UserData | null>(null)
-  // const [token, setToken] = useState<string | null>(null)
+  const [token, setToken] = useState<string | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
 
-  // useEffect(() => {})
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token")
+    const storedUser = localStorage.getItem("user")
+    if (storedToken && storedUser) {
+      setToken(storedToken)
+      setUser(JSON.parse(storedUser))
+    }
+    else {
+      setLoading(false)
+    }
+  }, [])
 
-  const handleAuthSuccess = (token: string, userData: any) => {
+  const handleAuthSuccess = (authToken: string, userData: any) => {
     setUser(userData)
+
+    setToken(authToken)
+    localStorage.setItem("token", authToken)
+    localStorage.setItem("user", JSON.stringify(userData))
+    setLoading(false)
+  }
+
+  const handleAuthLogout = () => {
+    setUser(null)
+    setToken(null)
+    localStorage.removeItem("user")
+    localStorage.removeItem("token")
+    console.log("Logout")
   }
 
   if (!user) {
@@ -25,9 +51,20 @@ export default function Home() {
   }
 
   return (
-    <div>
-      {user.name}
-      {user.email}
+    <div className="flex flex-cols justify-center items-center h-screen gap-2">
+      <Card className="p-7">
+        <div className="flex flex-cols justify-center items-center">
+          {user.name}
+        </div>
+        <div className="flex flex-cols justify-center items-center">
+          {user.email}
+        </div>
+        <div className="flex flex-cols justify-center items-center ">
+          <Button onClick={handleAuthLogout}>
+            Logout
+          </Button>
+        </div>
+      </Card>
     </div>
   )
 }
